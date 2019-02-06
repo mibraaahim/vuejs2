83a38c2cbd1f045a81af9d11c35409bfb7ecbec5
tiara venesa <tiaravenesa2@gmail.com>
2019-02-06 21:09:48 +0700

    vujs2

---
+++ README.md
@@ -0,0 +1,13 @@
+## Tentang Vue js
+Vue.js adalah suatu library Javascript yang digunakan untuk membangun antar muka sebuah website yang interaktif. Library dari Vue.js difokuskan hanya pada view layer dan sangat mudah diimplementasikan dan diintegrasikan dengan library lain ataupun juga dengan project yang sudah ada sebelumnya. Untuk mempelajari Vue ini setidaknya kita sudah mengerti dasar-dasar HTML, CSS dan juga Javascript.
+
+
+## Tugas : Membuat Form Pertanyaan
+Tugas ini adalah tugas membuat form yang berisi pertanyaan (soal) berbasis web menggunakan bahasa Vue.
+Saat membuka halaman website, terdapat textfield untuk mengisi nama dan terdapat 3 soal pilihan ganda
+Lalu, Anda dapat mengisi soal tersebut dengan meng-klik jawaban yang paling tepat.
+Ketika jawaban yang dipilih benar, maka nilai akan bertambah 1 dengan menggunakan fungsi if dan else.
+Jika jawaban salah, maka nilai tidak akan bertambah.
+Nilai akan tampil saat fungsi @click dijalankan.
+
+Muhammad Ibrahim / XII RPL 3 / 1617 116757
---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+   
+   <!--  <router-link to="/kelas">Data Kelas  </router-link>
+    <router-link to="/siswa">Data Siswa</router-link>
+    <router-view/><br><br> -->
+
+    <h1>{{ name }}</h1>
+    <input v-model="name" placeholder="Masukan Nama"><br><br>
+    <p>Nilai Anda : {{ total }}</p> 
+    <button v-on:click="button">Click Me!</button>
+
+    <p><b>1) Pulau Lombok berada di Provinsi ? </b></p>
+    <input type="radio" value="NTB" v-model="soal1">
+    Nusa Tenggara Barat &nbsp &nbsp
+    <input type="radio" value="NTT" v-model="soal1">
+    Nusa Tenggara Timur &nbsp &nbsp
+    <input type="radio" value="Kalbar" v-model="soal1">
+    Kalimantan Barat &nbsp &nbsp
+    <input type="radio" value="Kalteng" v-model="soal1">
+    Kalimantan Tengah 
+    <input type="radio" value="SulBarat" v-model="soal1">
+    Sulawesi Barat <br><br>
+
+    <p><b>2) 7 + 10 x 3 = </b></p>
+   <input type="radio" value="51" v-model="soal2">
+   51 &nbsp &nbsp
+   <input type="radio" value="37" v-model="soal2">
+   37 &nbsp &nbsp
+   <input type="radio" value="100" v-model="soal2">
+   100 &nbsp &nbsp
+   <input type="radio" value="53" v-model="soal2">
+   53 <br><br>
+
+   <p><b>3) Manakah hewan dibawah ini yang termasuk kedalam <i>OMNIVORA</i> ?</b></p>
+   <input type="radio" value="Macan" v-model="soal3">
+   Macan  &nbsp &nbsp
+   <input type="radio" value="Buaya" v-model="soal3">
+   Buaya  &nbsp &nbsp
+   <input type="radio" value="Badak" v-model="soal3">
+   Badak  &nbsp &nbsp
+   <input type="radio" value="Beruang" v-model="soal3">
+   Beruang &nbsp &nbsp
+   <input type="radio" value="Anoa" v-model="soal3">
+   Anoa 
+
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      name : '',
+      soal1 :'',
+      soal2 :'',
+      soal3 : '', 
+      total : 0
+    }
+    },
+  methods: {
+    button(){
+      if (this.soal1 == 'NTB' && this.soal2 == '37' && this.soal3 == 'Beruang') {
+        this.total=+3
+      }
+      else if(this.soal1 == 'NTB' && this.soal2 == '37') {
+        this.total=+2
+      }
+      else if(this.soal1 == 'NTB' && this.soal3 == 'Beruang') {
+        this.total=+2
+      }
+      else if(this.soal2 == '37' && this.soal3 == 'Beruang') {
+        this.total=+2
+      }
+      else if (this.soal1 == 'NTB') {
+        this.total=+1
+      }
+      else if(this.soal2 == '37') {
+        this.total=+1
+      }
+      else if (this.soal3 == 'Beruang') {
+        this.total=+1
+      }
+      else{this.total = 0}
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
